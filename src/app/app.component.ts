d069ff7a9588f757acf4f2151545515af551cc0e
Lucas Livchits <livchitsl@gmail.com>
2021-08-09 23:36:47 -0300

    Extact tab to a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
-  links: string[] = ['Usuarios', 'Posteos', 'Albumes'];
-  activeLink = this.links[0];
 }
