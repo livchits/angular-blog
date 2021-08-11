a65f04cc06f6275aa120605d9b42fd6e1296b769
Lucas Livchits <livchitsl@gmail.com>
2021-08-10 23:26:31 -0300

    Add fetch users in service

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,9 +1,21 @@
+import { User } from './user';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  private usersUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl); //agregar manejo de errores
+  }
 }
