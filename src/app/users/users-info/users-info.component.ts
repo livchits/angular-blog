b9ec81fc7eee7ead877cc711708c15b5b88dae7d
Lucas Livchits <livchitsl@gmail.com>
2021-08-11 18:34:44 -0300

    Add users info component

---
+++ src/app/users/users-info/users-info.component.ts
@@ -0,0 +1,27 @@
+import { UsersService } from './../users.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../user';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-users-info',
+  templateUrl: './users-info.component.html',
+  styleUrls: ['./users-info.component.scss'],
+})
+export class UsersInfoComponent implements OnInit {
+  constructor(
+    private usersService: UsersService,
+    private route: ActivatedRoute
+  ) {}
+
+  @Input() user?: User;
+
+  getUser(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.usersService.getUser(id).subscribe((user) => (this.user = user));
+  }
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+}
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,11 +1,21 @@
-import { AppRoutingModule } from './../app-routing.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import { AppRoutingModule } from './../app-routing.module';
+import { UsersInfoComponent } from './users-info/users-info.component';
 import { UsersListComponent } from './users-list/users-list.component';
 @NgModule({
-  declarations: [UsersListComponent],
-  imports: [CommonModule, MatDividerModule, MatListModule, AppRoutingModule],
+  declarations: [UsersListComponent, UsersInfoComponent],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatListModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
 })
 export class UsersModule {}
