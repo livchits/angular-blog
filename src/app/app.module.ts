8ce425ec43a6d8c6e1da268a5d8ee2078002c16b
Lucas Livchits <livchitsl@gmail.com>
2021-08-11 16:54:21 -0300

    Add users list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { UsersModule } from './users/users.module';
-import { UsersListComponent } from './users/users-list/users-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
@@ -10,8 +10,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TabsComponent } from './components/tabs/tabs.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
+import { UsersModule } from './users/users.module';
 @NgModule({
-  declarations: [AppComponent, TabsComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    TabsComponent,
+    HeaderComponent,
+    UsersListComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -20,6 +27,8 @@ import { TabsComponent } from './components/tabs/tabs.component';
     MatTabsModule,
     HttpClientModule,
     UsersModule,
+    MatListModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UsersListComponent } from './users-list/users-list.component';
-
 @NgModule({
-  declarations: [UsersListComponent],
+  declarations: [],
   imports: [CommonModule],
 })
 export class UsersModule {}
