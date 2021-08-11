fbcb7210551d284abc8355ffaed3834ec99ba391
Lucas Livchits <livchitsl@gmail.com>
2021-08-11 17:14:51 -0300

    Add routing for user info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
-import { UsersListComponent } from './users/users-list/users-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersInfoComponent } from './users/users-info/users-info.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
 
-const routes: Routes = [{ path: 'users', component: UsersListComponent }];
+const routes: Routes = [
+  { path: 'users', component: UsersListComponent },
+  { path: 'users/:id', component: UsersInfoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
