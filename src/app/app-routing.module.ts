65e4eebf90f3705a47cd138ca8025d4737a86b05
Lucas Livchits <livchitsl@gmail.com>
2021-08-11 23:42:32 -0300

    Add routing for posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersInfoComponent } from './users/users-info/users-info.component';
@@ -6,6 +7,7 @@ import { UsersListComponent } from './users/users-list/users-list.component';
 const routes: Routes = [
   { path: 'users', component: UsersListComponent },
   { path: 'users/:id', component: UsersInfoComponent },
+  { path: 'posts', component: PostsListComponent },
 ];
 
 @NgModule({
