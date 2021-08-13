19c704ef5dbf11ffab49bfed654014e76f6462cc
Lucas Livchits <livchitsl@gmail.com>
2021-08-12 23:36:58 -0300

    Add posts filter by user id on select change

--- src/app/posts/posts-list/posts-list.component.ts
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -11,16 +11,27 @@ export class PostsListComponent implements OnInit {
   constructor(private postsService: PostsService) {}
 
   posts: Post[] = [];
+  filteredPosts: Post[] = this.posts;
 
-  userIds: number[] = [1, 2, 3, 4];
+  userIds: number[] = [];
 
   getPosts(): void {
     this.postsService.getPosts().subscribe((posts) => {
       this.posts = posts;
+      this.filteredPosts = posts;
       this.userIds = this.posts.reduce((ids: number[], post) => {
         return ids.includes(post.userId) ? ids : [...ids, post.userId];
       }, []);
     });
+  }
+
+  handleSelectChange({ value }: { value: number | 'all' }) {
+    const filteredPostsByUserId =
+      value === 'all'
+        ? this.posts
+        : this.posts.filter((post) => post.userId === value);
+
+    this.filteredPosts = filteredPostsByUserId;
   }
 
   ngOnInit(): void {
