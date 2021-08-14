90097f78b76041fb583b9864f3abd1c51826745c
Lucas Livchits <livchitsl@gmail.com>
2021-08-13 21:57:55 -0300

    Add post info component

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,4 +1,5 @@
-import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
@@ -14,9 +15,10 @@ import { PostInfoComponent } from './post-info/post-info.component';
     CommonModule,
     AppRoutingModule,
     MatListModule,
-    MatDividerModule,
     MatSelectModule,
     MatInputModule,
+    MatCardModule,
+    MatButtonModule,
   ],
 })
 export class PostsModule {}
