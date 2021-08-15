fed288cdbdd0d5815d4ac6b4c19d8cd3808246ee
Lucas Livchits <livchitsl@gmail.com>
2021-08-14 23:34:53 -0300

    Add logic to filter posts by id in route

--- src/app/posts/posts-list/posts-list.component.ts
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../post';
 import { PostsService } from './../posts.service';
@@ -8,28 +9,38 @@ import { PostsService } from './../posts.service';
   styleUrls: ['./posts-list.component.scss'],
 })
 export class PostsListComponent implements OnInit {
-  constructor(private postsService: PostsService) {}
+  constructor(
+    private postsService: PostsService,
+    private route: ActivatedRoute
+  ) {}
 
   posts: Post[] = [];
   filteredPosts: Post[] = this.posts;
 
   userIds: number[] = [];
 
+  selectedUser: number | 'all' =
+    Number(this.route.snapshot.paramMap.get('id')) || 'all';
+
   getPosts(): void {
     this.postsService.getPosts().subscribe((posts) => {
       this.posts = posts;
-      this.filteredPosts = posts;
+      this.filteredPosts =
+        typeof this.selectedUser === 'number'
+          ? this.posts.filter((post) => post.userId === this.selectedUser)
+          : this.posts;
+
       this.userIds = this.posts.reduce((ids: number[], post) => {
         return ids.includes(post.userId) ? ids : [...ids, post.userId];
       }, []);
     });
   }
 
-  handleSelectChange({ value }: { value: number | 'all' }) {
+  handleSelectChange() {
     const filteredPostsByUserId =
-      value === 'all'
+      this.selectedUser === 'all'
         ? this.posts
-        : this.posts.filter((post) => post.userId === value);
+        : this.posts.filter((post) => post.userId === this.selectedUser);
 
     this.filteredPosts = filteredPostsByUserId;
   }
