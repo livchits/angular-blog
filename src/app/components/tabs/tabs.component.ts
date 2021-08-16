673f2574f9bbb5461913c37a21d3aeba8bb4c18d
Lucas Livchits <livchitsl@gmail.com>
2021-08-15 23:17:35 -0300

    Now tabs are active checking last part of the route

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,3 +1,4 @@
+import { UsersListComponent } from './../../users/users-list/users-list.component';
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
@@ -8,17 +9,20 @@ import { links } from 'src/app/constants/links';
   styleUrls: ['./tabs.component.scss'],
 })
 export class TabsComponent implements OnInit {
-  currentRoute: string = '';
+  lastWordRoute: string = '';
 
   tabLinks = links;
 
-  constructor(private router: Router) {
-    router.events
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events
       .pipe(filter((event: any) => event instanceof NavigationEnd))
       .subscribe((event) => {
-        this.currentRoute = event.url;
+        this.lastWordRoute = `/${event.url
+          .split('/')
+          .filter((item: string) => !parseInt(item))
+          .at(-1)}`;
       });
   }
-
-  ngOnInit(): void {}
 }
