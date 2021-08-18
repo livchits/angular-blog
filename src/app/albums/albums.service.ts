3f4407371cf19f09961ab89f489fbc9ab9c7adfa
Lucas Livchits <livchitsl@gmail.com>
2021-08-17 22:30:34 -0300

    Add service to get all albums

---
+++ src/app/albums/albums.service.ts
@@ -0,0 +1,21 @@
+import { Album } from './album';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AlbumsService {
+  constructor(private http: HttpClient) {}
+
+  private albumsUrl = 'https://jsonplaceholder.typicode.com/albums';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  getAlbums(): Observable<Album[]> {
+    return this.http.get<Album[]>(this.albumsUrl);
+  }
+}
