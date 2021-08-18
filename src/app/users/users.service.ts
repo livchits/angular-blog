7ffe692977d6a44f6feb475c84905c20584a1a85
Lucas Livchits <livchitsl@gmail.com>
2021-08-17 23:29:12 -0300

    Add method to get users album

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,7 +1,8 @@
-import { User } from './user';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Album } from '../albums/album';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,9 @@ export class UsersService {
   constructor(private http: HttpClient) {}
 
   private usersUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  private usersAlbumUrl = (userId: string) =>
+    `https://jsonplaceholder.typicode.com/users/${userId}/albums`;
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
@@ -22,5 +26,10 @@ export class UsersService {
   getUser(id: number): Observable<User | undefined> {
     const url = `${this.usersUrl}/${id}`;
     return this.http.get<User>(url); //agregar manejo de errores
+  }
+
+  getUsersAlbums(userId: string): Observable<Album[]> {
+    const usersAlbumUrl = this.usersAlbumUrl(userId);
+    return this.http.get<Album[]>(usersAlbumUrl);
   }
 }
